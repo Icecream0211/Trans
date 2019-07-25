125e899d44c87bcf2c55376bf5219d53ef08b5aa
Bing <icecream0211@gmail.com>
2019-07-25 23:48:09 +0800

    删除注解

--- src/main/java/com/bing/disruptor/DisruptorMain.java
+++ src/main/java/com/bing/disruptor/DisruptorMain.java
@@ -47,35 +47,5 @@ public class DisruptorMain {
 
         }
         disruptor.shutdown();
-
-
-//        EventFactory<MyEvent> myEventFactory = new MyEventFactory();
-//        Executor executor = Executors.newCachedThreadPool();
-//        int ringBufferSize = 32;
-//
-//        Disruptor<MyEvent> disruptor = new Disruptor<MyEvent>(myEventFactory,ringBufferSize,executor, ProducerType.SINGLE,new BlockingWaitStrategy());
-//        EventHandler<MyEvent> b = new MyEventHandlerB();
-//        EventHandler<MyEvent> c = new MyEventHandlerC();
-//        EventHandler<MyEvent> d = new MyEventHandlerD();
-//
-//        SequenceBarrier sequenceBarrier2 = disruptor.handleEventsWith(b,c).asSequenceBarrier();
-//        BatchEventProcessor processord = new BatchEventProcessor(disruptor.getRingBuffer(),sequenceBarrier2,d);
-//        disruptor.handleEventsWith(processord);
-////  disruptor.after(b,c).handleEventsWith(d);              // 此行能代替上两行的程序逻辑
-//        RingBuffer<MyEvent> ringBuffer = disruptor.start();    // 启动Disruptor
-//        for(int i=0; i<10; i++) {
-//            long sequence = ringBuffer.next();                 // 申请位置
-//            try {
-//                MyEvent myEvent = ringBuffer.get(sequence);
-//                myEvent.setValue(i);                           // 放置数据
-//            } finally {
-//                ringBuffer.publish(sequence);                  // 提交，如果不提交完成事件会一直阻塞
-//            }
-//            try{
-//                Thread.sleep(100);
-//            }catch (Exception e){
-//            }
-//        }
-//        disruptor.shutdown();
     }
 }
