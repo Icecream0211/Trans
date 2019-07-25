bae0e8cf644da028df84a5d0731a406fb6706984
Bing <icecream0211@gmail.com>
2019-07-26 00:37:27 +0800

    增加workerPool和EventGroup的区别说明

--- src/main/java/com/bing/disruptor/demo2/Demo2.java
+++ src/main/java/com/bing/disruptor/demo2/Demo2.java
@@ -9,6 +9,25 @@ import java.util.concurrent.Executors;
 
 /**
  * demo2仍然没有使用disruptor封装，使用workerPool替代了BatchEventProcesser
+ *
+ * 常用的方法是：disruptor.handleEventsWith(EventHandler ... handlers)，将多个EventHandler的实现类传入方法，封装成一个EventHandlerGroup，实现多消费者消费。
+ *
+ * disruptor的另一个方法是：disruptor.handleEventsWithWorkerPool(WorkHandler ... handlers)，将多个WorkHandler的实现类传入方法，封装成一个EventHandlerGroup实现多消费者消费。
+ *
+ * 两者共同点都是，将多个消费者封装到一起，供框架消费消息。
+ *
+ * 不同点在于，
+ *
+ * 1. 对于某一条消息m，handleEventsWith方法返回的EventHandlerGroup，Group中的每个消费者都会对m进行消费，各个消费者之间不存在竞争。handleEventsWithWorkerPool方法返回的EventHandlerGroup，Group的消费者对于同一条消息m不重复消费；也就是，如果c0消费了消息m，则c1不再消费消息m。
+ *
+ * 2. 传入的形参不同。对于独立消费的消费者，应当实现EventHandler接口。对于不重复消费的消费者，应当实现WorkHandler接口。
+ *
+ * 因此，根据消费者集合是否独立消费消息，可以对不同的接口进行实现。也可以对两种接口同时实现，具体消费流程由disruptor的方法调用决定。
+ * https://www.cnblogs.com/pku-liuqiang/p/8544700.html
+ *
+ * disruptor.handleEventsWith()和disruptor.handEventsWithWorkerPool组合，可以实现 多消费者全部消费，多消费者不重复消费的不同组合
+ *
+ *
  */
 public class Demo2 {
     public static void main(String[] args) throws InterruptedException {
